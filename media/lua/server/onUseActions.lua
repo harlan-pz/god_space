b0f35fb953ffc1530ee74f211d3b185d4da30ecc
zhangdongxing <zhangdongxing@mokahr.com>
2022-03-03 01:16:58 +0800

    武道家添加完成

--- media/lua/server/onUseActions.lua
+++ media/lua/server/onUseActions.lua
@@ -7,3 +7,53 @@ function Recipe.OnCreate.useRepairTool(items, result, player)
     player:getPrimaryHandItem():setCondition(player:getPrimaryHandItem():getConditionMax())
     player:Say(getText("IGUI_Use_Repair_Tool"))
 end
+
+function Recipe.OnCreate.exchangePrimaryWarrior(items, result, player)
+    if not player:getTraits():contains("warrior_primary") and not player:getTraits():contains("warrior_middle") and
+        not player:getTraits():contains("warrior_senior") then
+        player:getTraits():add("warrior_primary")
+        player:getModData().maxShieldValue = 50
+        player:getModData().shieldValue = 50
+        player:Say(getText("IGUI_Exchange_Primary_Warrior"))
+    end
+end
+
+function Recipe.OnCreate.exchangeMiddleWarrior(items, result, player)
+    if not player:getTraits():contains("warrior_middle") and not player:getTraits():contains("warrior_senior") and
+        player:getTraits():contains("warrior_primary") then
+        player:getTraits():add("warrior_middle")
+        player:getTraits():remove("warrior_primary")
+        player:getModData().maxShieldValue = 100
+        player:getModData().shieldValue = 100
+        player:Say(getText("IGUI_Exchange_Middle_Warrior"))
+    end
+
+end
+
+function Recipe.OnCreate.exchangeSeniorWarrior(items, result, player)
+    if not player:getTraits():contains("warrior_senior") and player:getTraits():contains("warrior_middle") then
+        player:getTraits():add("warrior_senior")
+        player:getTraits():remove("warrior_middle")
+        player:getModData().maxShieldValue = 200
+        player:getModData().shieldValue = 200
+        player:Say(getText("IGUI_Exchange_Senior_Warrior"))
+    end
+end
+
+function Recipe.OnCreate.repairShield20(items, result, player)
+    if player:getTraits():contains("lunhuizhe") and player:getModData().shieldValue < player:getModData().maxShieldValue then
+        player:getModData().shieldValue = player:getModData().shieldValue + 20 > player:getModData().maxShieldValue and
+                                              player:getModData().maxShieldValue or player:getModData().shieldValue + 20
+    end
+    player:Say(getText("IGUI_Max_Shield_Value") .. player:getModData().maxShieldValue)
+    player:Say(getText("IGUI_Shield_Value") .. player:getModData().shieldValue)
+end
+
+function Recipe.OnCreate.repairShield50(items, result, player)
+    if player:getTraits():contains("lunhuizhe") and player:getModData().shieldValue < player:getModData().maxShieldValue then
+        player:getModData().shieldValue = player:getModData().shieldValue + 50 > player:getModData().maxShieldValue and
+                                              player:getModData().maxShieldValue or player:getModData().shieldValue + 50
+    end
+    player:Say(getText("IGUI_Max_Shield_Value") .. player:getModData().maxShieldValue)
+    player:Say(getText("IGUI_Shield_Value") .. player:getModData().shieldValue)
+end
--- media/lua/shared/NPCs/GS_Init_Events.lua
+++ media/lua/shared/NPCs/GS_Init_Events.lua
@@ -4,6 +4,32 @@ require "MainCreationMethods"
 local function initGSTraits()
     TraitFactory.addTrait("lunhuizhe", getText("UI_trait_lunhuizhe"), 6, getText("UI_trait_lunhuizhe_desc"), false,
         false)
+    TraitFactory.addTrait("warrior_primary", getText("UI_trait_warrior_primary"), 6,
+        getText("UI_trait_warrior_primary_desc"), false, false)
+    TraitFactory.addTrait("warrior_middle", getText("UI_trait_warrior_middle"), 6,
+        getText("UI_trait_warrior_middle_desc"), false, false)
+    TraitFactory.addTrait("warrior_senior", getText("UI_trait_warrior_senior"), 6,
+        getText("UI_trait_warrior_senior_desc"), false, false)
+end
+
+local function initModData(_player)
+    local player = _player
+    local modData = player:getModData()
+    if player:HasTrait("lunhuizhe") and modData.maxShieldValue == nil then
+        modData.maxShieldValue = 20
+    end
+    if player:HasTrait("lunhuizhe") and modData.shieldValue == nil then
+        modData.shieldValue = modData.maxShieldValue
+    end
+    if player:HasTrait("warrior_primary") then
+        modData.maxShieldValue = 50
+    end
+    if player:HasTrait("warrior_primary") then
+        modData.maxShieldValue = 100
+    end
+    if player:HasTrait("warrior_primary") then
+        modData.maxShieldValue = 200
+    end
 end
 
 -- 初始化人物物品
@@ -11,8 +37,8 @@ local function initGSItems(playerObj)
     local inv = playerObj:getInventory()
     if playerObj:HasTrait("lunhuizhe") then
         inv:AddItem("GodSpace.God_Watch")
-        inv:AddItem("GodSpace.zomboid_medicine")
         inv:AddItems("GodSpace.god_point", 100)
+        inv:AddItems("GodSpace.side_quests_d", 3)
     end
 end
 
@@ -21,6 +47,15 @@ local function getItem(zombie)
     local player = getPlayer()
     if player:HasTrait("lunhuizhe") then
         player:getInventory():AddItems("GodSpace.god_point", ZombRand(5))
+        if ZombRand(100) < 6 then
+            player:getInventory():AddItems("GodSpace.side_quests_d", 1)
+        end
+        if ZombRand(1000) < 6 then
+            player:getInventory():AddItems("GodSpace.side_quests_c", 1)
+        end
+        if ZombRand(10000) < 6 then
+            player:getInventory():AddItems("GodSpace.side_quests_b", 1)
+        end
     end
 end
 
@@ -34,9 +69,25 @@ GS_Professions.DoProfessions = function()
     lunhuizhe_projession:addFreeTrait("lunhuizhe");
 end
 
-Events.OnGameBoot.Add(initGSTraits)
-Events.OnNewGame.Add(initGSItems)
-Events.OnZombieDead.Add(getItem)
+-- 护盾实现
+local function defendAttack(_player)
+    local player = _player
+    local modData = player:getModData()
+    if player:HasTrait("lunhuizhe") then
+        if player:getBodyDamage():getHealth() < 100 and modData.shieldValue > 0 then
+            player:getBodyDamage():RestoreToFullHealth()
+            modData.shieldValue = modData.shieldValue - ZombRand(5) > 0 and modData.shieldValue - ZombRand(5) or 0
+            player:Say(getText("IGUI_Shield_Value") .. modData.shieldValue)
+        end
+    end
+end
 
+Events.OnGameBoot.Add(initGSTraits)
 Events.OnGameBoot.Add(GS_Professions.DoProfessions);
+Events.OnNewGame.Add(initGSItems)
+Events.OnNewGame.Add(initModData)
 Events.OnCreateLivingCharacter.Add(GS_Professions.DoProfessions);
+Events.OnCreateLivingCharacter.Add(initModData);
+Events.OnZombieDead.Add(getItem)
+Events.OnPlayerUpdate.Add(defendAttack);
+
